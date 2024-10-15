8a6e4a7cd1699b71941f9faca14ec4325eafe3b5
Leone Varando <lvarando@gmail.com>
2024-10-14 20:40:54 -0500

    Added type validation requirements

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
-export default function currencyToNumber(currencyStr: string): number {
-  const cleanStr = currencyStr.replace(/[^0-9.-]/g, '');
-  const numberCon = parseFloat(cleanStr);
+export default function currencyToNumber(currencyStr: string | undefined): number {
+  const cleanStr = currencyStr?.replace(/[^0-9.-]/g, '');
+  const numberCon = typeof(cleanStr) === 'string' ? parseFloat(cleanStr) : NaN;
 
   if (isNaN(numberCon)) throw new Error('Invalid currency format');
 
