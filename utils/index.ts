bddbcdde4d2b3bc1d046f20c6a7f243f4bb2e570
Leone Varando <lvarando@gmail.com>
2024-10-26 02:33:05 -0500

    Added delay function

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,21 @@
-export default function currencyToNumber(currencyStr: string | undefined): number {
+export function currencyToNumber(currencyStr: string | undefined): number {
   const cleanStr = currencyStr?.replace(/[^0-9.-]/g, '');
   const numberCon = typeof(cleanStr) === 'string' ? parseFloat(cleanStr) : NaN;
 
   if (isNaN(numberCon)) throw new Error('Invalid currency format');
 
   return numberCon;
+}
+
+/**
+ * Used to delay function runs in an attempt to both mimic human behavior and not overload website being scraped.
+ * 
+ * Sleeps for a random amount of time between given minimum time and maximum time.
+ * 
+ * @param min number
+ * @param max number
+ * @returns Promise
+ */
+export function delay(min: number, max: number) {
+  return new Promise(res => setTimeout(res, Math.floor(Math.random() * (max - min + 1)) + min));
 }
