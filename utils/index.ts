5955d7a2062c040180034c7dec07db6eb5cbc2d8
Leone Varando <lvarando@gmail.com>
2024-11-08 13:02:37 -0600

    Added dateFormatting function

--- utils/index.ts
+++ utils/index.ts
@@ -2,7 +2,7 @@ export function currencyToNumber(currencyStr: string | undefined): number {
   const cleanStr = currencyStr?.replace(/[^0-9.-]/g, '');
   const numberCon = typeof(cleanStr) === 'string' ? parseFloat(cleanStr) : NaN;
 
-  if (isNaN(numberCon)) throw new Error('Invalid currency format');
+  if (isNaN(numberCon)) return NaN;
 
   return numberCon;
 }
@@ -18,4 +18,11 @@ export function currencyToNumber(currencyStr: string | undefined): number {
  */
 export function delay(min: number, max: number) {
   return new Promise(res => setTimeout(res, Math.floor(Math.random() * (max - min + 1)) + min));
+}
+
+export function formatDate(): string {
+  const curDate = new Date().toJSON().split('T')
+  const time = new Date().toLocaleTimeString('en-US').split(' ')[0];
+
+  return `${curDate[0]}_${time}`;
 }
