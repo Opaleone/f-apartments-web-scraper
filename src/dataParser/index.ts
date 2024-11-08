caad118ce07a594ab607c8a6fe572aef0e0449ba
Leone Varando <lvarando@gmail.com>
2024-11-08 13:02:07 -0600

    Redesigned dataParser
    Redesigned dataParser as previous design was flawed and not correctly creating different average objects based on square footage
    
    Also added console logs to show progress of program for end user

--- src/dataParser/index.ts
+++ src/dataParser/index.ts
@@ -1,5 +1,5 @@
-import { IFloorPlan, IProperty } from '../../Interfaces';
-import currencyToNumber from '../../utils';
+import { IDetails, IProperty } from '../../Interfaces';
+import { currencyToNumber } from '../../utils';
 
 /**
  * Used to parse through gathered apartment data and calculate average price for each floorplan according to square footage
@@ -8,13 +8,13 @@ import currencyToNumber from '../../utils';
  */
 export default function dataParser(data: IProperty[]) {
   const allAverages: IProperty[] = [];
-
+  
   for (let i = 0; i < data.length; i++) {
     let curProperty: IProperty = {
       propertyName: data[i].propertyName,
       address: data[i].address,
       floorPlans: []
-    }
+    };
 
     for (const floorPlan of data[i].floorPlans) {
       let floorPlanObj: any = {
@@ -22,34 +22,61 @@ export default function dataParser(data: IProperty[]) {
         beds: floorPlan.beds,
         baths: floorPlan.baths,
         averages: []
-      }
+      };
 
+      // Sort details by square footage
+      floorPlan.details.sort((a: IDetails, b: IDetails): number => {
+        const aSqFt = a.sqFt ? parseInt(a.sqFt.split(' ')[0]) : 0;
+        const bSqFt = b.sqFt ? parseInt(b.sqFt.split(' ')[0]) : 0;
+
+        return bSqFt - aSqFt;
+      });
+
+      // Initialize temporary object
       let tempObj = {
-        total: 0,
-        count: 0,
+        total: currencyToNumber(floorPlan.details[0].price),
+        count: 1,
         sqFt: floorPlan.details[0].sqFt
-      }
+      };
 
-      for (let j = 0; j < floorPlan.details.length; j++) {
-        if (floorPlan.details[j].sqFt !== tempObj.sqFt || j === floorPlan.details.length - 1) {
-          const detailPrep = {
+      for (let j = 1; j < floorPlan.details.length; j++) {
+        const price = currencyToNumber(floorPlan.details[j].price);
+        const sqFt = floorPlan.details[j].sqFt;
+
+        if (isNaN(price)) continue;
+
+        if (sqFt !== tempObj.sqFt) {
+          // Push the current group's average
+          floorPlanObj.averages.push({
             avgPrice: tempObj.total / tempObj.count,
             sqFt: tempObj.sqFt
-          }
+          });
 
-          floorPlanObj.averages.push(detailPrep);
-
-          tempObj.total = currencyToNumber(floorPlan.details[j].price);
-          tempObj.count = 1;
-          tempObj.sqFt = floorPlan.details[j].sqFt;
+          // Reset tempObj for the new group
+          tempObj = {
+            total: price,
+            count: 1,
+            sqFt: sqFt
+          };
+        } else {
+          // Accumulate prices within the same square footage group
+          tempObj.total += price;
+          tempObj.count++;
         }
-
-        tempObj.total += currencyToNumber(floorPlan.details[j].price);
-        tempObj.count++;
       }
+
+      // Push the last calculated average
+      floorPlanObj.averages.push({
+        avgPrice: tempObj.total / tempObj.count,
+        sqFt: tempObj.sqFt
+      });
+
       curProperty.floorPlans.push(floorPlanObj);
     }
+
     allAverages.push(curProperty);
   }
+
+  console.log(`Successfully parsed ${data.length} properties.`);
   return allAverages;
 }
