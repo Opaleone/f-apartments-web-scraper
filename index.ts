37a134e3b12831e904ff02b483270a8b4108f102
Leone Varando <lvarando@gmail.com>
2024-11-08 13:05:54 -0600

    Added various console logs
    Added various console logs to show progress of program to end user

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import readline from 'readline';
 import scrapeSite from './src/siteScraper';
 import dataParser from './src/dataParser';
 import fs from 'node:fs'
+import { formatDate } from './utils';
 
 const locationRegex = /^([a-z]-?){1,40}[-]{1}?([a-z]){1,2}$/g;
 
@@ -10,44 +11,45 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
-rl.question('What location do you want to search?\n', response => {
+rl.question('Welcome!\n\nWhat location do you want to search?\n', response => {
   if (!locationRegex.test(response)) {
     console.log('Incorrect Format. Try again!');
     rl.close();
   }
 
   scrapeSite(`https://www.apartments.com/${response}/`).then((res) => {
-    console.log('\n\nWriting to file...\n')
+    console.log('\n\nGenerating files...\n');
     try {
-      const fileName = `./propertyFloorPlans/${response}_${new Date().toJSON().split('T')[0]}.json`
+      const dateStr = formatDate();
+
+      const fileName = `${response}_${dateStr}.json`;
+
+      const propertyFolder = './propertyFloorPlans';
+      const averageFolder = './floorPlanAvgs';
 
       fs.mkdir('./propertyFloorPlans', e => {
-        if (e?.code === 'EEXIST') console.log('Folder already exists.');
-        else console.log(`propertyFloorPlans folder created successfully.`);
+        if (e?.code !== 'EEXIST') console.log(`propertyFloorPlans folder created successfully.`);
       })
       fs.mkdir('./floorPlanAvgs', e => {
-        if (e?.code === 'EEXIST') console.log('Folder already exists.');
-        else console.log(`floorPlanAvgs folder created successfully.`);
+        if (e?.code !== 'EEXIST') console.log(`floorPlanAvgs folder created successfully.`);
       })
 
-      fs.writeFile(fileName, JSON.stringify(res), (e) => {
+      fs.writeFile(`${propertyFolder}/${fileName}`, JSON.stringify(res), (e) => {
         console.log(e);
       })
-      console.log(`\nCity file successfully created. See file at ${fileName}.\n`);
+      console.log(`\nCity file successfully created. See file at ./propertyFloorPlans/${fileName}.\n`);
 
       console.log(`Parsing Property data...`);
       const allAverages = dataParser(res);
 
-      fs.writeFile(`./floorPlanAvgs/${response}_${new Date().toJSON().split('T')[0]}.json`, JSON.stringify(allAverages), (e) => {
+      fs.writeFile(`${averageFolder}/${fileName}`, JSON.stringify(allAverages), (e) => {
         console.log(e);
       })
-      console.log(`\nAverage file written. See file at ./floorPlanAvgs/${response}_${new Date().toJSON().split('T')[0]}.json.`);
-      console.log('\n\nExiting now. Goodbye!');
+      console.log(`\nAverage file written. See file at ./floorPlanAvgs/${fileName}`);
+      console.log('\n\nExiting now. Goodbye!\n');
     } catch (e) {
-      console.log(`Could not write file`, e);
+      console.log(`Could not generate file(s): `, e);
     }
   });
   rl.close();
 });
-
-
