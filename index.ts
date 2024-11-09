206a5e285a34fd86f53a2cecfa2d8d80bda57d55
Leone Varando <lvarando@gmail.com>
2024-11-09 17:51:35 -0600

    Added various fixes and optimizations

--- index.ts
+++ index.ts
@@ -1,56 +1,58 @@
 import readline from 'readline';
 import scrapeSite from './src/siteScraper';
 import dataParser from './src/dataParser';
-import fs from 'node:fs'
-import { formatDate } from './utils';
+import fs from 'node:fs';
+import path from 'node:path';
+import { formatCity, formatDate } from './utils';
 
 const locationRegex = /^([a-z]-?){1,40}[-]{1}?([a-z]){1,2}$/g;
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-})
+});
 
 rl.question('Welcome!\n\nWhat location do you want to search?\n', response => {
   if (!locationRegex.test(response)) {
     console.log('Incorrect Format. Try again!');
     rl.close();
+    return;
   }
 
   scrapeSite(`https://www.apartments.com/${response}/`).then((res) => {
+    if (!res) {
+      throw new Error('No Properties found!');
+    }
+
     console.log('\n\nGenerating files...\n');
     try {
       const dateStr = formatDate();
 
       const fileName = `${response}_${dateStr}.json`;
 
-      const propertyFolder = './propertyFloorPlans';
-      const averageFolder = './floorPlanAvgs';
+      const propertyFolder = path.resolve('floorplans');
+      const averageFolder = path.resolve('averages');
 
-      fs.mkdir('./propertyFloorPlans', e => {
-        if (e?.code !== 'EEXIST') console.log(`propertyFloorPlans folder created successfully.`);
-      })
-      fs.mkdir('./floorPlanAvgs', e => {
-        if (e?.code !== 'EEXIST') console.log(`floorPlanAvgs folder created successfully.`);
-      })
+      fs.mkdirSync(propertyFolder, { recursive: true });
+      fs.mkdirSync(averageFolder, { recursive: true });
 
-      fs.writeFile(`${propertyFolder}/${fileName}`, JSON.stringify(res), (e) => {
+      fs.writeFile(path.join(propertyFolder, fileName), JSON.stringify(res), (e) => {
         if (e) {
           console.log(e);
         }
-      })
-      console.log(`\nCity file successfully created. See file at ./propertyFloorPlans/${fileName}.\n`);
+      });
+      console.log(`File successfully created for ${formatCity(response)}. See file at ${path.join(propertyFolder, fileName)}\n`);
 
       console.log(`Parsing Property data...`);
       const allAverages = dataParser(res);
 
-      fs.writeFile(`${averageFolder}/${fileName}`, JSON.stringify(allAverages), (e) => {
+      fs.writeFile(path.join(averageFolder, fileName), JSON.stringify(allAverages), (e) => {
         if (e) {
           console.log(e);
         }
       })
-      console.log(`\nAverage file written. See file at ./floorPlanAvgs/${fileName}`);
-      console.log('\n\nExiting now. Goodbye!\n');
+      console.log(`\nAverage file written. See file at ${path.join(propertyFolder, fileName)}`);
+      console.log('\n\nExiting now.\nGoodbye!\n');
     } catch (e) {
       console.log(`Could not generate file(s): `, e);
     }
