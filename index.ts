2e3d590ab64353840d018f845f55e3f26e568086
Leone Varando <lvarando@gmail.com>
2024-11-11 08:48:23 -0600

    Added optimizations and console styling

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import readline from 'readline';
-import scrapeSite from './src/siteScraper';
-import dataParser from './src/dataParser';
 import fs from 'node:fs';
 import path from 'node:path';
-import { formatCity, formatDate } from './utils';
+import { styleText } from 'node:util';
+import { formatCity, formatDate, welcome } from './utils';
+import scrapeSite from './src/siteScraper';
+import dataParser from './src/dataParser';
 
 const locationRegex = /^([a-z]-?){1,40}[-]{1}?([a-z]){1,2}$/g;
 
@@ -12,14 +13,14 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Welcome!\n\nWhat location do you want to search?\n', response => {
-  if (!locationRegex.test(response)) {
-    console.log('Incorrect Format. Try again!');
-    rl.close();
-    return;
+console.log(styleText(['green'], welcome))
+
+rl.question('What location do you want to search?\n', city => {
+  if (!locationRegex.test(city)) {
+    throw new Error('Incorrect Format. Try again!');
   }
 
-  scrapeSite(`https://www.apartments.com/${response}/`).then((res) => {
+  scrapeSite(`https://www.apartments.com/${city}/`).then((res) => {
     if (!res) {
       throw new Error('No Properties found!');
     }
@@ -27,8 +28,9 @@ rl.question('Welcome!\n\nWhat location do you want to search?\n', response => {
     console.log('\n\nGenerating files...\n');
     try {
       const dateStr = formatDate();
+      const cityName = formatCity(city);
 
-      const fileName = `${response}_${dateStr}.json`;
+      const fileName = `${city}_${dateStr}.json`;
 
       const propertyFolder = path.resolve('floorplans');
       const averageFolder = path.resolve('averages');
@@ -41,18 +43,18 @@ rl.question('Welcome!\n\nWhat location do you want to search?\n', response => {
           console.log(e);
         }
       });
-      console.log(`File successfully created for ${formatCity(response)}. See file at ${path.join(propertyFolder, fileName)}\n`);
+      console.log(styleText(['green'], `File successfully created for ${cityName}. See file at ${path.join(propertyFolder, fileName)}\n`));
 
       console.log(`Parsing Property data...`);
-      const allAverages = dataParser(res);
+      const allAverages = dataParser(res, cityName);
 
       fs.writeFile(path.join(averageFolder, fileName), JSON.stringify(allAverages), (e) => {
         if (e) {
           console.log(e);
         }
       })
-      console.log(`\nAverage file written. See file at ${path.join(propertyFolder, fileName)}`);
-      console.log('\n\nExiting now.\nGoodbye!\n');
+      console.log(styleText(['green'], `\nAverage file written. See file at ${path.join(propertyFolder, fileName)}`));
+      console.log(styleText(['magentaBright'], `\n\nExiting now.\nGoodbye!`));
     } catch (e) {
       console.log(`Could not generate file(s): `, e);
     }
