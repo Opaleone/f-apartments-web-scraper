896824ae814bcbba5661333c703c6121a4051275
Leone Varando <LVARANDO@GMAIL.COM>
2024-11-19 09:02:17 -0600

    Incorporated server requests
    Changed file to add all necessary info to new server database instead of file creation.

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import fs from 'node:fs';
+// import fs from 'node:fs';
 import path from 'node:path';
+import axios from 'axios';
 import { styleText } from 'node:util';
-import { formatCity, formatDate, welcome } from './utils';
+import { formatCity, welcome } from './utils';
 import scrapeSite from './src/siteScraper';
 import dataParser from './src/dataParser';
 import { input } from '@inquirer/prompts';
@@ -17,40 +18,76 @@ console.log(styleText(['green'], welcome));
     throw new Error('Incorrect Format. Try again!');
   }
 
-  scrapeSite(`https://www.apartments.com/${city}/`).then((res) => {
+  scrapeSite(`https://www.apartments.com/${city}/`).then(async (res) => {
     if (!res) {
       throw new Error('No Properties found!');
     }
 
     console.log('\n\nGenerating files...\n');
     try {
-      const dateStr = formatDate();
-      const cityName = formatCity(city);
+      const baseURL = 'http://localhost:3001/api';
+      const cityAndState = formatCity(city);
+      const cityArr = cityAndState.split(', ');
 
-      const fileName = `${city}_${dateStr}.json`;
+      const propertyIds = [];
+      
+      for (let i = 0; i < res.length; i++) {
+        const floorplanIds: string[] = [];
 
-      const propertyFolder = path.resolve('floorplans');
-      const averageFolder = path.resolve('averages');
+        for (let j = 0; j < res[i].floorplans.length; j++ ) {
+          const curFloorplan = res[i].floorplans[j];
+          const detailsIds: string[] = [];
+          const averagesIds: string[] = [];
+          const parsedProperty = dataParser(curFloorplan.details);
 
-      fs.mkdirSync(propertyFolder, { recursive: true });
-      fs.mkdirSync(averageFolder, { recursive: true });
+          for (let k = 0; k < parsedProperty.length; k++) {
+            const average = await axios.post(`${baseURL}/averages/create`, {
+              price: parsedProperty[k].price,
+              sqFt: parsedProperty[k].sqFt,
+              whenAvailable: parsedProperty[k].whenAvailable
+            });
+            averagesIds.push(average.data._id);
+          }
 
-      fs.writeFile(path.join(propertyFolder, fileName), JSON.stringify(res), (e) => {
-        if (e) {
-          console.log(e);
+          if (curFloorplan.details) {
+            for (let k = 0; k < curFloorplan.details.length; k++) {
+              const detail = await axios.post(`${baseURL}/details/create`, curFloorplan.details[k]);
+  
+              detailsIds.push(detail.data._id);
+            }
+          }
+          const curFloorplanDB = await axios.post(`${baseURL}/floorplan/create`, {
+            name: curFloorplan.name,
+            beds: curFloorplan.beds,
+            baths: curFloorplan.baths,
+            details: detailsIds,
+            averages: averagesIds
+          })
+
+          floorplanIds.push(curFloorplanDB.data._id);
         }
-      });
-      console.log(styleText(['green'], `File successfully created for ${cityName}. See file at ${path.join(propertyFolder, fileName)}\n`));
 
-      console.log(`Parsing Property data...`);
-      const allAverages = dataParser(res, cityName);
+        const property = await axios.post(`${baseURL}/property/create`, {
+          propertyName: res[i].propertyName,
+          address: res[i].address,
+          phone: res[i].phone,
+          leasingOffice: res[i].leasingOffice,
+          floorplans: floorplanIds
+        })
 
-      fs.writeFile(path.join(averageFolder, fileName), JSON.stringify(allAverages), (e) => {
-        if (e) {
-          console.log(e);
-        }
+        propertyIds.push(property.data._id);
+      }
+
+      const cityDB = await axios.post(`${baseURL}/city/create`, {
+        cityName: cityArr[0],
+        state: cityArr[1],
+        properties: propertyIds
       })
-      console.log(styleText(['green'], `\nAverage file written. See file at ${path.join(propertyFolder, fileName)}`));
+
+      if (cityDB) {
+        console.log(styleText(['green'], `${cityDB.data.cityName}, ${cityDB.data.state} created with it's properties successfully!\n`))
+      }
+
       console.log(styleText(['magentaBright'], `\n\nExiting now.\nGoodbye!`));
     } catch (e) {
       console.error(`Could not generate file(s): \n`, e);
